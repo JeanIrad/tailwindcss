2d375793f9070c283950f310b46776372e63e41b
Iradukunda Jean De Dieu <jado.milton@gmail.com>
2024-06-16 11:02:16 +0200

    Delete node_modules directory

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const os = require('os')
-console.log(os.machine())
-console.log(os.hostname())
-console.log(os.networkInterfaces())
+// const os = require("os");
+// console.log(os.machine());
+// console.log(os.hostname());
+// console.log(os.networkInterfaces());
+
+const n = 5;
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    console.log("*");
+  }
+  //   console.log("\n");
+}
+console.log("hello \nworld");
--- tailwindSetup.md
+++ tailwindSetup.md
@@ -1,13 +1,23 @@
 ## Setting up
 
 ### simple setup:
+
 ```
 npx tailwindcss-cli build input.css -o output.css
 ```
+
 ### Dev setup:
+
 ```
 1. npm init -y
 2. npm i -D vite tailwindcss postcss autoprefixer
 3. npx tailwindcss init -p
 
 ```
+
+## custom configuration
+
+``
+run: npx tailwindcss init --full
+
+``
